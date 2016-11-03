7ee92b71a3c97da3d70dec8d9791308a8d61f3aa
DiegoT <tellodiego@yahoo.com>
2016-11-03 05:37:53 +0000

    Simple socketio server.

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+// var messages = [{
+//   idMensaje: "1",
+//   titulo: "Alerta nro 12",
+//   descripcion: "Algun fenomeno"
+// }];
+var messages = [];
+
+io.on('connection', function(socket) {
+  console.log('Alguien se ha conectado con Sockets');
+  // socket.emit('messages', messages);
+  //
+  // socket.on('new-message', function(data) {
+  //   var objeto = JSON.parse(data);
+  //   console.log(objeto);
+  //   //messages = objeto;
+  //   //messages.push(objeto);
+  //
+  //   io.sockets.emit('messages', objeto);
+  //});
+});
+
+server.listen(4040, function() {
+  console.log("Servidor socket corriendo en http://localhost:4040");
+});
